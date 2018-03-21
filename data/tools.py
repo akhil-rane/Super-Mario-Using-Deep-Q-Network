fc403ae106c0a0f321d2c8b223ffd66db3c107ea
Akhil Rane <akhil131192@gmail.com>
2018-03-21 13:11:07 -0400

    Adding Mario play randomization

---
+++ data/tools.py
@@ -0,0 +1,217 @@
+__author__ = 'justinarmstrong'
+
+import os
+import pygame as pg
+import keyboard
+import numpy
+
+keybinding = {
+    'action':pg.K_s,
+    'jump':pg.K_a,
+    'left':pg.K_LEFT,
+    'right':pg.K_RIGHT,
+    'down':pg.K_DOWN
+}
+
+
+
+class Control(object):
+
+
+    """Control class for entire project. Contains the game loop, and contains
+    the event_loop which passes events to States as needed. Logic for flipping
+    states is also found here."""
+    def __init__(self, caption):
+        self.screen = pg.display.get_surface()
+        self.done = False
+        self.clock = pg.time.Clock()
+        self.caption = caption
+        self.fps = 60
+        self.show_fps = False
+        self.current_time = 0.0
+        self.keys = pg.key.get_pressed()
+        self.state_dict = {}
+        self.state_name = None
+        self.state = None
+        self.choice = [True,False]
+        self.directions = ['left','right','up','down']
+        self.key_configurations = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    def setup_states(self, state_dict, start_state):
+        self.state_dict = state_dict
+        self.state_name = start_state
+        self.state = self.state_dict[self.state_name]
+
+    def update(self):
+        self.current_time = pg.time.get_ticks()
+        if self.state.quit:
+            self.done = True
+        elif self.state.done:
+            self.flip_state()
+        self.state.update(self.screen, self.keys, self.current_time)
+
+    def flip_state(self):
+        previous, self.state_name = self.state_name, self.state.next
+        persist = self.state.cleanup()
+        self.state = self.state_dict[self.state_name]
+        self.state.startup(self.current_time, persist)
+        self.state.previous = previous
+
+
+    def event_loop(self):
+
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT:
+                self.done = True
+            elif event.type == pg.KEYDOWN:
+                self.keys = pg.key.get_pressed()
+                self.toggle_show_fps(event.key)
+            elif event.type == pg.KEYUP:
+                self.keys = pg.key.get_pressed()
+
+
+            self.state.get_event(event)
+
+
+    def toggle_show_fps(self, key):
+        if key == pg.K_F5:
+            self.show_fps = not self.show_fps
+            if not self.show_fps:
+                pg.display.set_caption(self.caption)
+
+
+    def main(self):
+        """Main loop for entire program"""
+
+        frame_id=0;
+
+        frame = 0;
+
+        while not self.done:
+
+            #Randomization Code Begins
+
+            direction = numpy.random.choice(self.directions)
+
+            s_press = numpy.random.choice(self.choice)
+
+            a_press = numpy.random.choice(self.choice)
+
+            if not self.keys[pg.K_RETURN] and frame == 0:
+
+                if direction =='left':
+                    self.key_configurations[273] = 0
+                    self.key_configurations[274] = 0
+                    self.key_configurations[275] = 0
+                    self.key_configurations[276] = 1
+                elif direction == 'right':
+                    self.key_configurations[273] = 0
+                    self.key_configurations[274] = 0
+                    self.key_configurations[275] = 1
+                    self.key_configurations[276] = 0
+                elif direction == 'down':
+                    self.key_configurations[273] = 0
+                    self.key_configurations[274] = 1
+                    self.key_configurations[275] = 0
+                    self.key_configurations[276] = 0
+                elif direction == 'up':
+                    self.key_configurations[273] = 1
+                    self.key_configurations[274] = 0
+                    self.key_configurations[275] = 0
+                    self.key_configurations[276] = 0
+
+                if s_press:
+                    self.key_configurations[300] = 1
+                else:
+                    self.key_configurations[300] = 0
+
+                if a_press:
+                    self.key_configurations[97] = 1
+                else:
+                    self.key_configurations[97] = 0
+
+                pg.image.save(self.screen, "frames/frame_"+str(frame_id)+".png")
+
+                self.keys = self.key_configurations
+
+            frame_id = frame_id+1;
+
+            frame = frame+1
+
+            if frame == 10:
+                frame = 0
+
+            #Randomization Code Ends
+
+            self.event_loop()
+            self.update()
+            pg.display.update()
+            self.clock.tick(self.fps)
+            if self.show_fps:
+                fps = self.clock.get_fps()
+                with_fps = "{} - {:.2f} FPS".format(self.caption, fps)
+                pg.display.set_caption(with_fps)
+
+
+class _State(object):
+    def __init__(self):
+        self.start_time = 0.0
+        self.current_time = 0.0
+        self.done = False
+        self.quit = False
+        self.next = None
+        self.previous = None
+        self.persist = {}
+
+    def get_event(self, event):
+        pass
+
+    def startup(self, current_time, persistant):
+        self.persist = persistant
+        self.start_time = current_time
+
+    def cleanup(self):
+        self.done = False
+        return self.persist
+
+    def update(self, surface, keys, current_time):
+        pass
+
+
+
+def load_all_gfx(directory, colorkey=(255,0,255), accept=('.png', 'jpg', 'bmp')):
+    graphics = {}
+    for pic in os.listdir(directory):
+        name, ext = os.path.splitext(pic)
+        if ext.lower() in accept:
+            img = pg.image.load(os.path.join(directory, pic))
+            if img.get_alpha():
+                img = img.convert_alpha()
+            else:
+                img = img.convert()
+                img.set_colorkey(colorkey)
+            graphics[name]=img
+    return graphics
+
+
+def load_all_music(directory, accept=('.wav', '.mp3', '.ogg', '.mdi')):
+    songs = {}
+    for song in os.listdir(directory):
+        name,ext = os.path.splitext(song)
+        if ext.lower() in accept:
+            songs[name] = os.path.join(directory, song)
+    return songs
+
+
+def load_all_fonts(directory, accept=('.ttf')):
+    return load_all_music(directory, accept)
+
+
+def load_all_sfx(directory, accept=('.wav','.mpe','.ogg','.mdi')):
+    effects = {}
+    for fx in os.listdir(directory):
+        name, ext = os.path.splitext(fx)
+        if ext.lower() in accept:
+            effects[name] = pg.mixer.Sound(os.path.join(directory, fx))
+    return effects
